b629318a832bfdd58018a6c3bb0553b567c677b9
Kevin Lam <kevinlam@Klam-macbook.local>
2017-01-23 20:34:27 -0800

    brush status a bit bugged, got the mouse drag to work tho

--- pixel.js
+++ pixel.js
@@ -1,17 +1,66 @@
-
-
-
-
 // initial setup for the entire grid div
 let setup = document.createElement('div');
 setup.style.width = "50%"
 setup.style.margin = "0 auto"
 setup.style.textAlign = "center"
-let color = "nothing"
+let color = "toast";
+let downMouse;
+
+// still attempting brush color status
 
 
+directions()
+desiredColor()
+document.body.append(document.createElement('br'))
+document.body.append(document.createElement('br'))
 paletteSetup()
-gridSetup(24) // Feel free to put in whatever grid size you want
+gridSetup(26.07) // Feel free to put in whatever grid size you want
+
+
+
+
+function directions() {
+  // directions/documentation
+  //
+  // Note: I definitely could have done this in html but I wanted to try making it all
+  // through JS DOM Creation
+  let h1 = document.createElement('h1');
+  h1.innerHTML = "Pixel Pixel"
+  h1.style.textAlign = "center"
+  h1.style.fontFamily = "VT323, monospace"
+  document.body.append(h1)
+
+  let p = document.createElement('p');
+  p.innerHTML = "Directions: Click on the color palette on the left to set a color! If there's missing a color that you want, type in the name or hexcode of the color desired!"
+  p.style.textAlign = "center"
+  p.style.fontFamily = "VT323, monospace"
+  document.body.append(p)
+}
+
+function desiredColor() {
+  // still attempting user-desired additional colors
+  let div = document.createElement('div')
+  div.classList.add('container')
+  // div.classList.add('valign-wrapper')
+  let input = document.createElement('input')
+  let button = document.createElement('button')
+  div.append(button)
+  div.append(input)
+  document.body.append(div)
+
+  document.querySelector('input').setAttribute('id', 'color-text')
+  document.querySelector('input').setAttribute('type', 'color')
+
+  document.querySelector('button').setAttribute('id', 'moar-colors')
+  document.getElementById('moar-colors').innerHTML = "Submit"
+  document.getElementById('moar-colors').addEventListener('click', function() {
+    let text = document.getElementById('color-text').value;
+    color = text.toString()
+    let span = document.createElement('span')
+    span.innerText = `Your brush is currently: ${color}`
+    document.body.append(span)
+  })
+}
 
 function gridSetup(num) {
   for (i = 0; i < num * num; i++) {
@@ -20,17 +69,29 @@ function gridSetup(num) {
     gridBlock.style.paddingBottom = "2%"
     gridBlock.style.border = "1px solid black"
     gridBlock.style.width = "5%"
-    gridBlock.style.height = "5%"
-    gridBlock.innerHTML = "&nbsp;"
-    gridBlock.addEventListener("click", function() {
-      gridBlock.style.backgroundColor = color
+    gridBlock.innerHTML = ""
+    gridBlock.addEventListener("mouseup", function() {
+      downMouse = false;
+      this.style.backgroundColor = color
     })
+
+    gridBlock.addEventListener('mousedown', function() {
+      downMouse = true;
+    })
+
+    gridBlock.addEventListener('mouseenter', function() {
+      if (downMouse) {
+        this.style.backgroundColor = color
+      }
+    })
+
     setup.append(gridBlock)
   }
   document.body.appendChild(setup)
 }
 
 function paletteSetup() {
+  // original array of colors to choose from
   let arrayOfColors = ["red", "blue", 'black', "yellow", "orange", "green", 'white', "violet", "indigo"]
   let palettePlate = document.createElement('div')
   palettePlate.style.margin = '0 auto'
@@ -38,63 +99,21 @@ function paletteSetup() {
   palettePlate.style.width = "25%"
   palettePlate.style.display = "inline-block"
   palettePlate.style.float = "left"
+    // palette.style.display = "block"
   arrayOfColors.forEach(function(element) {
     let palette = document.createElement('div')
     palette.style.textAlign = "center"
-    // palette.style.display = "block"
     palette.style.float = "right"
     palette.style.width = "250px"
     palette.style.margin = "0 auto"
     palette.style.backgroundColor = element
     palette.innerHTML = "&nbsp;"
+
     palette.addEventListener("click", function() {
       color = element
     })
+
     palettePlate.append(palette)
   })
   document.body.appendChild(palettePlate)
 }
-
-// directions/documentation
-//
-// Note: I definitely could have done this in html but I wanted to try making it all
-// through JS DOM Creation
-let h1 = document.createElement('h1');
-h1.innerHTML = "Pixel Pixel"
-h1.style.textAlign = "center"
-h1.style.fontFamily = "VT323, monospace"
-document.body.append(h1)
-
-let p = document.createElement('p');
-p.innerHTML = "Directions: Click on the color palette on the left to set a color! If there's missing a color that you want. Type in the name or hexcode of the color desired!"
-p.style.textAlign = "center"
-p.style.fontFamily = "VT323, monospace"
-document.body.append(p)
-
-
-// still attempting user-desired additional colors
-let input = document.createElement('input')
-document.body.append(input)
-document.querySelector('input').setAttribute('type', 'text')
-document.querySelector('input').setAttribute('placeholder', 'Desired Color!')
-document.querySelector('input').setAttribute('id', 'color-text')
-
-let button = document.createElement('button')
-document.body.append(button)
-document.querySelector('button').setAttribute('id', 'moar-colors')
-document.getElementById('moar-colors').innerHTML = "Submit"
-document.getElementById('moar-colors').addEventListener('click', function() {
-  let text = document.getElementById('color-text').value;
-  arrayOfColors.push(text.toString())
-})
-
-let br = document.createElement('br')
-let br1 = document.createElement('br1')
-document.body.append(br)
-document.body.append(br1)
-
-// still attempting brush color status
-let span = document.createElement('span')
-span.innerHTML = "Your brush is currently: "
-span.innerHTML += color
-document.body.append(span)
